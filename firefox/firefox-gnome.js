02c4113315c52013e41e2beaa82df9b0a4eb50c8
Kevin (brugr) <git@brugr.com>
2022-10-13 04:59:45 +0200

    add optional configs for lepton and firefox gnome

---
+++ firefox/firefox-gnome.js
@@ -0,0 +1,14 @@
+//Firefox GNOME overridess - https://github.com/rafaelmardojai/firefox-gnome-theme/
+
+
+// Enable customChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Set UI density to normal
+user_pref("browser.uidensity", 0);
+
+// Enable SVG context-propertes
+user_pref("svg.context-properties.content.enabled", true);
+
+//Tab tree prop
+user_pref("gnomeTheme.extensions.tabCenterReborn", true);
